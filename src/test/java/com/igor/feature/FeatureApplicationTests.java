5f05b9e9390d2e27323e0107cb97e9c878dac18d
Ihor Dmitriiev <i.dmitriiev@spd-ukraine.com>
2020-11-14 08:35:55 +0200

    add test

--- src/test/java/com/igor/feature/FeatureApplicationTests.java
+++ src/test/java/com/igor/feature/FeatureApplicationTests.java
@@ -1,13 +1,53 @@
 package com.igor.feature;
 
-import org.junit.jupiter.api.Test;
+import com.igor.feature.config.Features;
+import com.igor.feature.controller.MessageController;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class FeatureApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @MockBean
+  private Features features;
 
+  @Autowired
+  private MessageController messageController;
+
+  @BeforeAll
+  static void setUpClass() {
+    UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken("test", "test");
+    SecurityContextHolder.getContext().setAuthentication(auth);
+  }
+
+  @BeforeEach
+  void setUp() {
+    Mockito.reset(features);
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(FeatureEnabledOrDisabledArgumentProvider.class)
+  void shouldReturnMessage(boolean isQcEnabled) {
+    // given
+    when(features.isQcEnabled()).thenReturn(isQcEnabled);
+
+    // when
+    String message = messageController.getMessage();
+    System.out.println(message);
+
+    // then
+    assertThat(message).isEqualTo(isQcEnabled ? "QC" : "NON-QC");
+  }
 }
